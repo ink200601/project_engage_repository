89fd37798a42c75da058d3001fb518326b2f16a6
Emily <tweetychrip@gmail.com>
2022-12-21 09:54:00 -0800

    Fight camera

---
+++ base_game/camera/fight_cam.gd
@@ -0,0 +1,37 @@
+extends Camera3D
+
+
+const MIN_X := -20
+const MAX_X := 20
+const MIN_Y := -15
+const MAX_Y := 15
+const MIN_Z := 20
+const MAX_Z := 50
+const Z_FACTOR := 4
+
+var combatants: Array
+
+
+func _ready() -> void:
+	combatants = $"../Combatants".get_children()
+
+
+func _physics_process(delta: float) -> void:
+	if combatants.size() == 0:
+		return
+	var average := Vector2()
+	for c in combatants:
+		average += Vector2(c.position.x, c.position.y)
+	average /= combatants.size()
+	position.x = clamp(average.x, MIN_X, MAX_X)
+	position.y = clamp(average.y, MIN_Y, MAX_Y)
+	if combatants.size() < 2:
+		return
+	var longest: float = combatants[0].position.distance_squared_to(combatants[1].position)
+	for i in combatants.size():
+		for j in range(i, combatants.size()):
+			var c: float = combatants[i].position.distance_squared_to(combatants[j].position)
+			if c > longest:
+				longest = c
+	longest = sqrt(longest) * Z_FACTOR
+	position.z = clamp(longest, MIN_Z, MAX_Z)
